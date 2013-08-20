ddc6f339b4702361cecba0b4926f37cdb76616d2
Jan Dudulski <jan@dudulski.pl>
2013-08-20 23:58:46 +0200

    Explicte set game number

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,19 @@ class GamesController < ApplicationController
 
   expose(:games) { Game.includes(results: [:house, :player]).includes(:game_version).order(number: :desc) }
   expose(:game, attributes: :game_params)
+  expose(:last_game_number) { Game.maximum(:number) || 0 }
 
   def index
     respond_to do |format|
       format.html
       format.json { render json: games.extend(GamesRepresenter) }
     end
+  end
+
+  def new
+    game.number = last_game_number + 1
+
+    respond_with game
   end
 
   def create
@@ -34,6 +41,6 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:date, :game_version_id, :comment)
+    params.require(:game).permit(:number, :date, :game_version_id, :comment)
   end
 end
