97bd7c17fdd151b89fe42e8830e196a12651c838
Jan Dudulski <jan@dudulski.pl>
2013-08-21 23:03:55 +0200

    Display a house icon on games page

--- app/representers/house_representer.rb
+++ app/representers/house_representer.rb
@@ -2,7 +2,13 @@ require 'roar/representer/json'
 
 module HouseRepresenter
   include Roar::Representer::JSON
+  include Roar::Representer::JSON::HAL::Links
 
   property :id
   property :name
+  property :icon
+
+  def icon
+    self.name.downcase
+  end
 end
