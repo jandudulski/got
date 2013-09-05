a83c15e13190ad18ba093d1ea4cc88894479ff1c
Jan Dudulski <jan@dudulski.pl>
2013-09-05 12:57:55 +0200

    Players page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < ApplicationController
   respond_to :html, :json
+  layout false
 
   expose(:players) { Player.includes(:results).order(name: :asc) }
   expose(:player)
@@ -8,6 +9,13 @@ class PlayersController < ApplicationController
     respond_to do |format|
       format.html
       format.json { render json: players.extend(PlayersRepresenter) }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: player.extend(PlayerRepresenter) }
     end
   end
 end
