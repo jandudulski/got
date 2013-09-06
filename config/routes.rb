0d3d0c9fb5b83b1c67a2562558ae6c664daec55a
Jan Dudulski <jan@dudulski.pl>
2013-09-06 20:38:35 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Got::Application.routes.draw do
   resources :game_versions, only: [:index]
-  resources :games, except: [:destroy] do
+  resources :games, only: [:index, :new, :create, :update, :edit] do
     resources :results, except: [:index]
   end
-
+  resources :houses, only: [:index]
   resources :players, only: [:index, :show]
 
   root 'pages#init'
